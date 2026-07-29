6fbc70f2b486041cfb14ae47134e14a0937de5a1
Jaden Dennis <115289951+jadenjdennis@users.noreply.github.com>
2026-07-29 17:52:48 -0400

    Added comments for readability

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import cv2
 
+#All this class does is initialize the camera and set it up for reading frames. It also has a function to read the current frame and return it.
 class Camera:
     def __init__(self, camera_index = 0):
         self.cap = cv2.VideoCapture(camera_index)
@@ -7,6 +8,8 @@ class Camera:
         if not self.cap.isOpened():
             exit()
 
+    #All this does is read the current fram from the camera and return it.
+    #If not successful, then it prints and error message and exits the program.
     def readFrame(self):
         isSuccess, frame = self.cap.read()
 
